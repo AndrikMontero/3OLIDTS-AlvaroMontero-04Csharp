cc6079f2a5c05258e0f97e1463a98a8eae871ce2
Andrik Montero <andrik.gm42@gmail.com>
2025-09-06 12:27:24 -0600

    Agregar archivos de proyecto.

---
+++ 3OLIDTS-AlvaroMontero-04Csharp/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace _3OLIDTS_AlvaroMontero_04Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbApellidos = new System.Windows.Forms.Label();
+            this.lbTelefono = new System.Windows.Forms.Label();
+            this.lbEstatura = new System.Windows.Forms.Label();
+            this.lbEdad = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbApellidos = new System.Windows.Forms.TextBox();
+            this.tbTelefono = new System.Windows.Forms.TextBox();
+            this.tbEstatura = new System.Windows.Forms.TextBox();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.gbGenero = new System.Windows.Forms.GroupBox();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.rbFemenino = new System.Windows.Forms.RadioButton();
+            this.btGuardar = new System.Windows.Forms.Button();
+            this.btLimpiar = new System.Windows.Forms.Button();
+            this.gbGenero.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Font = new System.Drawing.Font("Copperplate Gothic Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombre.Location = new System.Drawing.Point(106, 65);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(83, 17);
+            this.lbNombre.TabIndex = 0;
+            this.lbNombre.Text = "Nombre :";
+            this.lbNombre.Click += new System.EventHandler(this.lbNombre_Click);
+            // 
+            // lbApellidos
+            // 
+            this.lbApellidos.AutoSize = true;
+            this.lbApellidos.Font = new System.Drawing.Font("Copperplate Gothic Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbApellidos.Location = new System.Drawing.Point(106, 127);
+            this.lbApellidos.Name = "lbApellidos";
+            this.lbApellidos.Size = new System.Drawing.Size(103, 17);
+            this.lbApellidos.TabIndex = 1;
+            this.lbApellidos.Text = "Apellidos :";
+            this.lbApellidos.Click += new System.EventHandler(this.lbApellidos_Click);
+            // 
+            // lbTelefono
+            // 
+            this.lbTelefono.AutoSize = true;
+            this.lbTelefono.Font = new System.Drawing.Font("Copperplate Gothic Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTelefono.Location = new System.Drawing.Point(105, 189);
+            this.lbTelefono.Name = "lbTelefono";
+            this.lbTelefono.Size = new System.Drawing.Size(104, 17);
+            this.lbTelefono.TabIndex = 2;
+            this.lbTelefono.Text = "Telefono : ";
+            this.lbTelefono.Click += new System.EventHandler(this.lbTelefono_Click);
+            // 
+            // lbEstatura
+            // 
+            this.lbEstatura.AutoSize = true;
+            this.lbEstatura.Font = new System.Drawing.Font("Copperplate Gothic Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEstatura.Location = new System.Drawing.Point(113, 248);
+            this.lbEstatura.Name = "lbEstatura";
+            this.lbEstatura.Size = new System.Drawing.Size(96, 17);
+            this.lbEstatura.TabIndex = 3;
+            this.lbEstatura.Text = "Estatura :";
+            this.lbEstatura.Click += new System.EventHandler(this.lbEstatura_Click);
+            // 
+            // lbEdad
+            // 
+            this.lbEdad.AutoSize = true;
+            this.lbEdad.Font = new System.Drawing.Font("Copperplate Gothic Light", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEdad.Location = new System.Drawing.Point(116, 303);
+            this.lbEdad.Name = "lbEdad";
+            this.lbEdad.Size = new System.Drawing.Size(60, 17);
+            this.lbEdad.TabIndex = 4;
+            this.lbEdad.Text = "Edad :";
+            this.lbEdad.Click += new System.EventHandler(this.lbEdad_Click);
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(221, 60);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(162, 26);
+            this.tbNombre.TabIndex = 5;
+            this.tbNombre.TextChanged += new System.EventHandler(this.tbNombre_TextChanged);
+            // 
+            // tbApellidos
+            // 
+            this.tbApellidos.Location = new System.Drawing.Point(221, 122);
+            this.tbApellidos.Name = "tbApellidos";
+            this.tbApellidos.Size = new System.Drawing.Size(162, 26);
+            this.tbApellidos.TabIndex = 6;
+            this.tbApellidos.TextChanged += new System.EventHandler(this.tbApellidos_TextChanged);
+            // 
+            // tbTelefono
+            // 
+            this.tbTelefono.Location = new System.Drawing.Point(221, 184);
+            this.tbTelefono.Name = "tbTelefono";
+            this.tbTelefono.Size = new System.Drawing.Size(162, 26);
+            this.tbTelefono.TabIndex = 7;
+            this.tbTelefono.TextChanged += new System.EventHandler(this.tbTelefono_TextChanged);
+            // 
+            // tbEstatura
+            // 
+            this.tbEstatura.Location = new System.Drawing.Point(221, 243);
+            this.tbEstatura.Name = "tbEstatura";
+            this.tbEstatura.Size = new System.Drawing.Size(162, 26);
+            this.tbEstatura.TabIndex = 8;
+            this.tbEstatura.TextChanged += new System.EventHandler(this.tbEstatura_TextChanged);
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(221, 298);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(162, 26);
+            this.tbEdad.TabIndex = 9;
+            this.tbEdad.TextChanged += new System.EventHandler(this.tbEdad_TextChanged);
+            // 
+            // gbGenero
+            // 
+            this.gbGenero.BackColor = System.Drawing.Color.Transparent;
+            this.gbGenero.Controls.Add(this.rbFemenino);
+            this.gbGenero.Controls.Add(this.rbMasculino);
+            this.gbGenero.Location = new System.Drawing.Point(84, 377);
+            this.gbGenero.Name = "gbGenero";
+            this.gbGenero.Size = new System.Drawing.Size(335, 98);
+            this.gbGenero.TabIndex = 10;
+            this.gbGenero.TabStop = false;
+            this.gbGenero.Text = "Genero";
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbMasculino.Location = new System.Drawing.Point(16, 43);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(114, 24);
+            this.rbMasculino.TabIndex = 0;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbFemenino
+            // 
+            this.rbFemenino.AutoSize = true;
+            this.rbFemenino.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbFemenino.Location = new System.Drawing.Point(186, 43);
+            this.rbFemenino.Name = "rbFemenino";
+            this.rbFemenino.Size = new System.Drawing.Size(113, 24);
+            this.rbFemenino.TabIndex = 1;
+            this.rbFemenino.TabStop = true;
+            this.rbFemenino.Text = "Femenino";
+            this.rbFemenino.UseVisualStyleBackColor = true;
+            // 
+            // btGuardar
+            // 
+            this.btGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btGuardar.ForeColor = System.Drawing.Color.Green;
+            this.btGuardar.Location = new System.Drawing.Point(132, 523);
+            this.btGuardar.Name = "btGuardar";
+            this.btGuardar.Size = new System.Drawing.Size(92, 50);
+            this.btGuardar.TabIndex = 11;
+            this.btGuardar.Text = "Guardar";
+            this.btGuardar.UseVisualStyleBackColor = false;
+            this.btGuardar.Click += new System.EventHandler(this.btGuardar_Click);
+            // 
+            // btLimpiar
+            // 
+            this.btLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btLimpiar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btLimpiar.Location = new System.Drawing.Point(293, 523);
+            this.btLimpiar.Name = "btLimpiar";
+            this.btLimpiar.Size = new System.Drawing.Size(93, 50);
+            this.btLimpiar.TabIndex = 12;
+            this.btLimpiar.Text = "Limpiar";
+            this.btLimpiar.UseVisualStyleBackColor = false;
+            this.btLimpiar.Click += new System.EventHandler(this.btLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(518, 644);
+            this.Controls.Add(this.btLimpiar);
+            this.Controls.Add(this.btGuardar);
+            this.Controls.Add(this.gbGenero);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.tbEstatura);
+            this.Controls.Add(this.tbTelefono);
+            this.Controls.Add(this.tbApellidos);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.lbEdad);
+            this.Controls.Add(this.lbEstatura);
+            this.Controls.Add(this.lbTelefono);
+            this.Controls.Add(this.lbApellidos);
+            this.Controls.Add(this.lbNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbGenero.ResumeLayout(false);
+            this.gbGenero.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbApellidos;
+        private System.Windows.Forms.Label lbTelefono;
+        private System.Windows.Forms.Label lbEstatura;
+        private System.Windows.Forms.Label lbEdad;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbApellidos;
+        private System.Windows.Forms.TextBox tbTelefono;
+        private System.Windows.Forms.TextBox tbEstatura;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.GroupBox gbGenero;
+        private System.Windows.Forms.RadioButton rbFemenino;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.Button btGuardar;
+        private System.Windows.Forms.Button btLimpiar;
+    }
+}
+
---
+++ 3OLIDTS-AlvaroMontero-04Csharp/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //Libreria para lectura y escritura de archivos
+
+namespace _3OLIDTS_AlvaroMontero_04Csharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbNombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbApellidos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbApellidos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbTelefono_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbTelefono_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbEstatura_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbEstatura_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbEdad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbEdad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLimpiar_Click(object sender, EventArgs e)
+        {
+         //Codigo para funcionalidad del boton limpiar, borra todo
+            tbApellidos.Clear();
+            tbEdad.Clear(); 
+            tbNombre.Clear();
+            tbEstatura.Clear();
+            tbTelefono.Clear();
+            rbFemenino.Checked = false;
+            rbMasculino.Checked = false;
+
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+         //Codigo para funcionalidad del boton guardar, guarda lo de los textbox
+            string nombres = tbNombre.Text;
+            string apellidos = tbApellidos.Text;
+            string edad = tbEdad.Text;
+            string estatura = tbEstatura.Text;
+            string telefono = tbTelefono.Text;
+            string genero = "";
+            if(rbFemenino.Checked)
+            {
+                genero = "Femenino";
+            } else if(rbMasculino.Checked)
+            {
+                genero = "Masculino";
+            }
+            string datos = $"Nombre : {nombres}\n\rApellidos :  {apellidos}\r\n" +
+                $"Edad : {edad}\r\nEstatura : {estatura}\r\nTelefono : {telefono}\r\n" +
+                $"Genero : {genero}\r\n";
+            string ruta ="C:\\Users\\KatPC\\Documents\\Tareastercersemestre\\3OLIDTS2025.txt";
+            //string ruta = @"C:\Users\KatPC\Documents\Tareastercersemestre\3OLIDTS2025.txt";
+            bool archivoExiste = File.Exists(ruta);
+            using (StreamWriter writer = new StreamWriter(ruta, true)) 
+            {
+                if (archivoExiste)
+                {
+                    writer.WriteLine();
+                }
+                writer.WriteLine(datos);
+            }
+            MessageBox.Show(datos, "Valores ingresados",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
